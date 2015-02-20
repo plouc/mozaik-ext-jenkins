156ebd78118b3768f85a4cb327042651f0a494e1
Raphaël Benitte <benitteraphael@gmail.com>
2015-02-20 09:39:45 +0100

    Add npm pre publish script to package.json + .npmignore

--- src/client.js
+++ src/client.js
@@ -2,73 +2,88 @@ var request = require('superagent');
 require('superagent-bluebird-promise');
 var Promise = require('bluebird');
 
-function buildRequest(url) {
-    return request.get(url)
-        .auth(
-            config.api.jenkins.auth.user,
-            config.api.jenkins.auth.password
-        )
-        .promise()
-    ;
-}
 
-module.exports = {
-    jobs: function () {
-        return buildRequest(config.api.jenkins.baseUrl + '/api/json?tree=jobs[name,lastBuild[number,building,timestamp,result]]&pretty=true')
-            .then(function (res) {
-                return res.body.jobs;
-            })
-        ;
-    },
-    job: function (params) {
-        return buildRequest(config.api.jenkins.baseUrl +  '/job/' + params.job + '/api/json?pretty=true&depth=10&tree=builds[number,duration,result,builtOn,timestamp,id,building]')
-            .then(function (res) {
-                return res.body.builds;
-            })
-        ;
-    },
-    jobBuild: function (params) {
-        return buildRequest(config.api.jenkins.baseUrl + '/job/' + params.job + '/' + params.buildNumber + '/api/json?pretty=true')
-            .then(function (res) {
-                return res.body;
-            })
-        ;
-    },
-    view: function (params) {
-        return buildRequest(config.api.jenkins.baseUrl + '/view/' + params.view + '/api/json?pretty=true&depth=1')
-            .then(function (res) {
-                var view = res.body;
-                var jobs = view.jobs;
 
-                var builds = [];
+var client = function (mozaik) {
+    function buildRequest(path) {
 
-                // Fetch builds details
-                jobs.forEach(function (job) {
-                    [
-                        'lastBuild',
-                        'lastFailedBuild',
-                        'lastSuccessfulBuild'
-                    ].forEach(function (buildType) {
-                        if (job[buildType]) {
-                            builds.push(
-                                module.exports.jobBuild({
-                                    job:         job.name,
-                                    buildNumber: job[buildType].number
-                                })
-                                .then(function (build) {
-                                    job[buildType] = build;
-                                })
-                            );
-                        }
-                    });
-                });
+        var url = config.get('jenkins.baseUrl' + path)
 
-                return Promise.all(builds)
-                    .then(function () {
-                        return view;
-                    })
-                ;
-            })
+        return request.get(url)
+            .auth(
+                config.api.jenkins.auth.user,
+                config.api.jenkins.auth.password
+            )
+            .promise()
         ;
     }
+
+    return {
+        jobs() {
+            return buildRequest(config.api.jenkins.baseUrl + '/api/json?tree=jobs[name,lastBuild[number,building,timestamp,result]]&pretty=true')
+                .then(function (res) {
+                    return res.body.jobs;
+                })
+            ;
+        },
+
+        job(params) {
+            return buildRequest(config.api.jenkins.baseUrl +  '/job/' + params.job + '/api/json?pretty=true&depth=10&tree=builds[number,duration,result,builtOn,timestamp,id,building]')
+                .then(function (res) {
+                    return res.body.builds;
+                })
+            ;
+        },
+
+        jobBuild(params) {
+            return buildRequest(config.api.jenkins.baseUrl + '/job/' + params.job + '/' + params.buildNumber + '/api/json?pretty=true')
+                .then(function (res) {
+                    return res.body;
+                })
+            ;
+        },
+
+        view(params) {
+            return buildRequest(config.api.jenkins.baseUrl + '/view/' + params.view + '/api/json?pretty=true&depth=1')
+                .then(function (res) {
+                    var view = res.body;
+                    var jobs = view.jobs;
+
+                    var builds = [];
+
+                    // Fetch builds details
+                    jobs.forEach(function (job) {
+                        [
+                            'lastBuild',
+                            'lastFailedBuild',
+                            'lastSuccessfulBuild'
+                        ].forEach(function (buildType) {
+                            if (job[buildType]) {
+                                builds.push(
+                                    module.exports.jobBuild({
+                                        job:         job.name,
+                                        buildNumber: job[buildType].number
+                                    })
+                                    .then(function (build) {
+                                        job[buildType] = build;
+                                    })
+                                );
+                            }
+                        });
+                    });
+
+                    return Promise.all(builds)
+                        .then(function () {
+                            return view;
+                        })
+                    ;
+                })
+            ;
+        }
+    };
 };
+
+
+
+
+module.exports = client;
--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,7 @@
-var components = {
+module.exports = {
     Jobs:               require('./Jobs.jsx'),
     JobStatus:          require('./JobStatus.jsx'),
     JobBuilds:          require('./JobBuilds.jsx'),
     JobBuildsHistogram: require('./JobBuildsHistogram.jsx'),
     View:               require('./View.jsx')
 };
-
-require('mozaik/browser')
-    .add('jenkins.jobs',                 components.Jobs)
-    .add('jenkins.job_status',           components.JobStatus)
-    .add('jenkins.job_builds',           components.JobBuilds)
-    .add('jenkins.job_builds_histogram', components.JobBuildsHistogram)
-    .add('jenkins.view',                 components.View)
-;
-
-module.exports = components;
