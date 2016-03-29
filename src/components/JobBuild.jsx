dd80770c51a576f7d3f66d724e80ea095eeee7ea
Macklin Underdown <macklin@detroitlabs.com>
2016-03-29 16:43:25 -0400

    Add building status to JobBuild
    This commit adds support for a currently building Jenkins job
    for the JobBuild widget.
    
    Resolves #14

--- src/components/JobBuild.jsx
+++ src/components/JobBuild.jsx
@@ -6,11 +6,11 @@ class JobBuild extends Component {
     render() {
         const { build } = this.props;
 
-        const classes = `list__item list__item--with-status list__item--with-status--${ build.result.toLowerCase() }`;
+        const classes = `list__item list__item--with-status list__item--with-status--${ this.buildResult().toLowerCase() }`;
 
         return (
             <div className={classes}>
-                #{build.number} {build.result}&nbsp;
+                #{build.number} {this.buildResult()}&nbsp;
                 <time className="list__item__time">
                     <i className="fa fa-clock-o" />&nbsp;
                     {moment(build.timestamp, 'x').fromNow()}
@@ -18,12 +18,27 @@ class JobBuild extends Component {
             </div>
         );
     }
+
+    buildResult() {
+        const { build } = this.props;
+
+        if (build.result) {
+            return build.result;
+        }
+
+        if (build.building) {
+            return 'BUILDING';
+        }
+
+        return 'UNKNOWN';
+    }
 }
 
 JobBuild.displayName = 'JobBuild';
 
 JobBuild.propTypes = {
     build: PropTypes.shape({
+        building:  PropTypes.bool.isRequired,
         number:    PropTypes.number.isRequired,
         result:    PropTypes.string.isRequired,
         timestamp: PropTypes.number.isRequired
