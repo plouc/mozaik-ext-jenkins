5d83688557a6c4a9c6c1509f5193d17d19751f37
Macklin Underdown <macklin@detroitlabs.com>
2016-04-13 14:44:06 -0400

    Extract getBuildStatus function

--- src/components/JobBuild.jsx
+++ src/components/JobBuild.jsx
@@ -1,36 +1,23 @@
 import React, { Component, PropTypes } from 'react'; // eslint-disable-line no-unused-vars
 import moment                          from 'moment';
+import { getBuildStatus }              from './util';
 
 
 class JobBuild extends Component {
     render() {
         const { build } = this.props;
 
-        const classes = `list__item list__item--with-status list__item--with-status--${ this.buildResult().toLowerCase() }`;
+        const classes = `list__item list__item--with-status list__item--with-status--${ getBuildStatus(build).toLowerCase() }`;
 
         return (
             <div className={classes}>
-                #{build.number} {this.buildResult()}&nbsp;
+                #{build.number} {getBuildStatus(build)}&nbsp;
                 <time className="list__item__time">
                     <i className="fa fa-clock-o" />&nbsp;
                     {moment(build.timestamp, 'x').fromNow()}
                 </time>
             </div>
         );
-    }
-
-    buildResult() {
-        const { build } = this.props;
-
-        if (build.result) {
-            return build.result;
-        }
-
-        if (build.building) {
-            return 'BUILDING';
-        }
-
-        return 'UNKNOWN';
     }
 }
 
